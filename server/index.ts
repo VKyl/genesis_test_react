bc5762d26844557d2cdeba4949017bacb253a39d
vkyl <viktor.kyliukh@gmail.com>
2025-03-27 17:50:57 +0200

    ts for node.js

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+
 const httpServer = createServer();
 const io = new Server(httpServer, {});
+
 io.on("connection", (socket) => {
-    socket.on("message", (data) => {
-        console.log(data);
-    });
+  socket.on("message", (data) => {
+    console.log(data)
+  })
 });
+
 httpServer.listen(3000);
